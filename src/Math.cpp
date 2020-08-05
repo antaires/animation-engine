f80f04873a10ac2756f973e1d5c16848ee37da55
antaires <11vdlo@gmail.com>
2020-08-05 18:33:43 +0200

    add addiontal vec3 funcs

--- src/Math.cpp
+++ src/Math.cpp
@@ -87,6 +87,22 @@ float operator*(const Vec3& a, const Vec3& b)
   return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
+float operator==(const Vec3& a, const Vec3& b)
+{
+    Vec3 diff(a - b);
+    return lenSqr(diff) < VEC_EPSILON;
+}
+
+float operator!=(const Vec3& a, const Vec3& b)
+{
+  return !(a == b);
+}
+
+float dot(const Vec3& a, const Vec3& b)
+{
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
 float lenSqr(const Vec3& v)
 {
   return (v.x * v.x) + (v.y * v.y) + (v.z * v.z);
@@ -139,4 +155,86 @@ float angle(const Vec3& a, const Vec3& b)
   float dot = a.x * b.x + a.y * b.y + a.z * b.z;
 
   acosf(dot / (std::sqrtf(aLenSq) * std::sqrtf(bLenSq)));
+}
+
+// project a onto b (get part of a in direction of b)
+Vec3 project(const Vec3& a, const Vec3& b)
+{
+  // a dot b divided by len b
+  float bLen = len(b);
+  if (bLen < VEC_EPSILON)
+  {
+    return Vec3();
+  }
+  float scale = dot(a, b) / bLen;
+  return b * scale;
+}
+
+Vec3 reject(const Vec3& a, const Vec3& b)
+{
+  Vec3 projection = project(a, b);
+  return a - projection;
+}
+
+// bounce reflection, where a 'bounces' off of
+// plane and b is the plane's normal
+Vec3 reflect(const Vec3& a, const Vec3& b)
+{
+  float bLen = len(b);
+  if(bLen < VEC_EPSILON)
+  {
+    return Vec3();
+  }
+  float scale = dot(a, b) / bLen;
+  Vec3 projection2 = b * (scale * 2);
+  return a - projection2;
+}
+
+Vec3 cross(const Vec3& a, const Vec3& b)
+{
+  return Vec3(
+    a.y * b.z - a.z * b.y
+    , a.z * b.x - a.x * b.z
+    , a.x * b.y - a.y * b.x
+  );
+}
+
+Vec3 lerp(const Vec3& s, const Vec3& e, float t)
+{
+  // straight line (so lenght is not constant in relation to t)
+  // always takes the shortest path from one vec to another
+  return Vec3(
+    s.x + (e.x - s.x) * t
+    , s.y + (e.y - s.y) * t
+    , s.z + (e.z - s.z) * t
+  );
+}
+
+Vec3 slerp(const Vec3& s, const Vec3& e, float t)
+{
+  // arc between two vectors
+  if (t < 0.01f)
+  {
+    return lerp(s, e, t);
+  }
+
+  Vec3 from = normalized(s);
+  Vec3 to   = normalized(e);
+  float theta = angle(from, to);
+  float sin_theta = sinf(theta);
+  float a = sinf((1.0f - t) * theta) / sin_theta;
+  float b = sinf(t * theta) / sin_theta;
+  return from * a + to * b;
+}
+
+Vec3 nlerp(const Vec3& s, const Vec3& e, float t)
+{
+  // approximates slerp, but isn't constant velocity
+  // faster to compute
+  Vec3 linear(
+    s.x + (e.x - s.x) * t
+    , s.y + (e.y - s.y) * t
+    , s.z + (e.z - s.z) * t
+  );
+  return normalized(linear);
 }
--- src/Math.h
+++ src/Math.h
@@ -4,19 +4,23 @@ const float VEC_EPSILON = 0.000001f;
 
 // the anonymouse union allows Vec3 to be accessed via .x .y .z and
 // as a contiguous array using .v
-struct Vec2
+template<typename T>
+struct TVec2
 {
   union {
     struct {
-      float x;
-      float y;
+      T x;
+      T y;
     };
-    float v[2];
+    T v[2];
   };
-  inline Vec2() : x(0.0f), y(0.0f) {}
-  inline Vec2(float _x, float _y):x(_x), y(_y) {}
-  inline Vec2(float *fv): x(fv[0]), y(fv[1]) {}
+  inline TVec2() : x(0.0f), y(0.0f) {}
+  inline TVec2(T _x, T _y):x(_x), y(_y) {}
+  inline TVec2(T *fv): x(fv[0]), y(fv[1]) {}
 };
+typedef TVec2<float> Vec2;
+typedef TVec2<int> iVec2;
+
 // basic vector2 operations
 Vec2 operator+(const Vec2& a, const Vec2& b);
 Vec2 operator-(const Vec2& a, const Vec2& b);
@@ -47,8 +51,17 @@ Vec3 operator+(const Vec3& a, const Vec3& b);
 Vec3 operator-(const Vec3& a, const Vec3& b);
 Vec3 operator*(const Vec3& v, float f);
 float operator*(const Vec3& a, const Vec3& b);
+float operator==(const Vec3& a, const Vec3& b);
+float operator!=(const Vec3& a, const Vec3& b);
+float dot(const Vec3& a, const Vec3& b);
 float lenSqr(const Vec3& v);
 float len(const Vec3& v);
 void normalize(Vec3& v);       // alters input vec
 Vec3 normalized(const Vec3& v); // return new
 float angle(const Vec3& a, const Vec3& b);
+Vec3 project(const Vec3& a, const Vec3& b);
+Vec3 reject(const Vec3& a, const Vec3& b);
+Vec3 reflect(const Vec3& a, const Vec3& b);
+Vec3 lerp(const Vec3& a, const Vec3& b, float t);
+Vec3 slerp(const Vec3& a, const Vec3& b, float t);
+Vec3 nlerp(const Vec3& a, const Vec3& b, float t);
